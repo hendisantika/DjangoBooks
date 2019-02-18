29a3234e9039d520f77566b9260424f334e73edb
hendisantika <hendisantika@yahoo.co.id>
2019-02-19 05:54:53 +0700

    #5 - Modify urls.py file

--- DjangoBooks/urls.py
+++ DjangoBooks/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Examples:
+    # url(r'^$', 'web.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index_redirect, name='index_redirect'),
+    url(r'^books/', include('books.urls')),
 ]
