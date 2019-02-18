8efffbe3a439efbc40c3fc0e6104deab0323aa73
hendisantika <hendisantika@yahoo.co.id>
2019-02-19 05:57:54 +0700

    #8 - Add books/templates/books/index.html file

--- DjangoBooks/settings.py
+++ DjangoBooks/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'DjangoBooks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'books/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
